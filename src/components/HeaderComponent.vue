aff8250a072a78715762b64c8a7f3fb78191c911
Samuelec14 <samuconf14@gmail.com>
2023-07-24 16:29:47 +0200

    header structure commit

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,4 +1,5 @@
 <script>
+
 export default {
     name: "HeaderComponent",
     data() {
@@ -15,7 +16,58 @@ export default {
 <template>
 
     <header>
-    HEADER
+
+                <!-- CONTACTS HEADER SECTION -->
+            <section id="contacts-heading-sec">
+                <div class="container">
+                    <div class="row">
+
+                        <div class="col">
+                            LINKS 
+                        </div>
+
+                        <div class="col text-end">
+                            LOGIN
+                        </div>
+
+                    </div>
+                </div>
+            </section>
+
+            <!-- LOGO AND SEARCHBAR HEADER SECTION -->
+            <section id="searchbar-sec">
+                <div class="container">
+                    <div class="row align-items-center">
+
+                        <div class="col">
+                            LOGO
+                        </div>
+
+                        <div class="col text-end">
+                            SEARCHBAR
+                        </div>
+
+                    </div>
+                </div>
+            </section>
+
+            <!-- NAVBAR AND CART HEADER SECTION -->
+            <section id="navbar-sec">
+                <div class="container">
+                    <div class="row">
+
+                        <div class="col-8">
+                            NAVBAR
+                        </div>
+
+                        <div class="col-4 text-end">
+                            CHART
+                        </div>
+
+                    </div>
+                </div>
+            </section>
+
     </header>
 
 </template>
@@ -23,8 +75,27 @@ export default {
 <style lang="scss" scoped>
 
 header{
-    padding: 40px;
-    text-align: center;
+    // debug
+    min-height: 200px;
+
+
+    #contacts-heading-sec{
+        border-bottom: 1px solid rgb(48, 48, 51);
+        line-height: 55px;
+    }
+
+    #searchbar-sec{
+        border-bottom: 1px solid rgb(48, 48, 51);
+        
+
+        .row{
+            height: 150px;
+        }
+    }
+
+    #navbar-sec{
+        line-height: 70px;
+    }
 }
 
 </style>
